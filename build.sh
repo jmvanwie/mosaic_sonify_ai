f87defb2073f4b449710e30bab4b4a576fad5bea
vanwiejm <vanwiejm@gmail.com>
2025-07-08 11:02:34 -0400

    Chore: Add diagnostic step to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Print the contents of requirements.txt to the build log
+echo "--- Displaying contents of requirements.txt ---"
+cat requirements.txt
+echo "--- End of requirements.txt ---"
+
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
